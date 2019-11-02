94805827316a2870566820409014e2522818f3f9
Andrew A <AndrewADev@gmail.com>
2019-11-02 22:13:06 +0100

    Add basic e2e tests
    Add some basic e2e tests to cover a simple game scenario.
    
    Update protractor so that it uses recent Chrome versions.
    
    Make sure e2e tests run in CI

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -11,4 +11,22 @@ describe('workspace-project App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welcome to ngTicTacToe!');
   });
+
+  it('should start with in-progress status', () => {
+    page.navigateTo();
+    expect(page.getCurrentStatusDashboardText()).toContain('In-progress');
+  });
+
+  it('should play a normal game', () => {
+    page.navigateTo();
+    // P1
+    page.clickSpaceByIndex(0);
+    page.clickSpaceByIndex(1);
+    // P1
+    page.clickSpaceByIndex(4);
+    page.clickSpaceByIndex(2);
+    // P1 - should be winning play
+    page.clickSpaceByIndex(8);
+    expect(page.getCurrentStatusDashboardText()).toContain('Game over');
+  });
 });
